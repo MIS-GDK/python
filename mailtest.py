fe48ec01e3ea3e93e11c6976368e406e881fa6a2
gaodakui <gaodakui@tp-link.com.cn>
2018-04-28 10:32:50 +0800

    module modify python Thread 2.0

---
+++ mailtest.py
@@ -0,0 +1,19 @@
+from email.mime.text import MIMEText
+import smtplib
+
+msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+
+# 输入Email地址和口令:
+from_addr = input('From: ')
+password = input('Password: ')
+# 输入收件人地址:
+to_addr = input('To: ')
+# 输入SMTP服务器地址:
+smtp_server = input('SMTP server: ')
+
+# SMTP协议默认端口是465
+server = smtplib.SMTP(smtp_server, 465) 
+server.set_debuglevel(1)
+server.login(from_addr, password)
+server.sendmail(from_addr, [to_addr], msg.as_string())
+server.quit()
