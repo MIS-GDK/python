e9d602916f1c396d03660062d820101a70978e44
mis-gdk <1530951440@qq.com>
2020-04-10 16:10:11 +0800

    递归反转链表

---
+++ list_soution.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def __init__(self):
+        self.head = None
+
+    def reverseList(self, head: ListNode) -> ListNode:
+        if head.next == None:
+            return head
+        new_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return new_head
+
+    def addnode(self, val):
+        temp = ListNode(val)
+        temp.next = self.head
+        self.head = temp
+
+        return self.head
+
+
+mylist = Solution()
+
+for i in range(1, 6):
+    # print(i)
+    my_head = mylist.addnode(i)
+
+new_my_head = mylist.reverseList(my_head)
+
+print("------------------------")
+
+while new_my_head is not None:
+    print(new_my_head.val)
+    new_my_head = new_my_head.next
